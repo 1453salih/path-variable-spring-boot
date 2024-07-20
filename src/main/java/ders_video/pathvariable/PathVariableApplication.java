c941e0a8ef5119621dea348eb7b689968ee49f84
Salih Korkmaz <salihkorkmaz108478@gmail.com>
2024-07-20 17:12:19 +0300

    f_1

---
+++ src/main/java/ders_video/pathvariable/PathVariableApplication.java
@@ -0,0 +1,13 @@
+package ders_video.pathvariable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathVariableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ders_video/pathvariable/PathVariableApplicationTests.java
@@ -0,0 +1,13 @@
+package ders_video.pathvariable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathVariableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
